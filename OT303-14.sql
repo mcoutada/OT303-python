1eac792818ea129d6aaa7d467efa05b533a828ee
Mariano Ismael Coutada <mcoutada@gmail.com>
2022-09-15 12:50:29 -0300

    formatting

--- OT303-14.sql
+++ OT303-14.sql
@@ -1,4 +1,4 @@
-/*
+/*************************************************************************
 https://alkemy-labs.atlassian.net/browse/OT303-14
 
 COMO: Analista de datos
@@ -25,30 +25,30 @@ location
 email
 Aclaración: Las tablas tienen dos universidades cada una. No hace falta interpretar datos que no parezcan lógicos como fechas de nacimiento y de inscripción fuera del rango de interés. Lo importante es traer toda la información de la base de datos en las fechas especificadas y cada tarea se debe ejecutar 5 veces antes de fallar.
     
-*/
+*************************************************************************/
 
 -- Univ. Nacional Del Comahue
 SELECT
-    f.universidad AS university,
-    f.carrera AS career,
-    f.fecha_de_inscripcion AS inscription_date,
-    split_part(f.name, ' ', 1) AS first_name,
-    split_part(f.name, ' ', 2) AS last_name,
-    f.sexo AS gender,
-    date_part('year',age(TO_DATE(f.fecha_nacimiento,'YYYY-MM-DD'))) AS age,
-    f.codigo_postal AS postal_code,
+    fc.universidad AS university,
+    fc.carrera AS career,
+    fc.fecha_de_inscripcion AS inscription_date,
+    SPLIT_PART(fc.name, ' ', 1) AS first_name,
+    SPLIT_PART(fc.name, ' ', 2) AS last_name,
+    fc.sexo AS gender,
+    DATE_PART('year', AGE(TO_DATE(fc.fecha_nacimiento,'YYYY-MM-DD'))) AS age,
+    fc.codigo_postal AS postal_code,
     l.localidad AS location,
-    f.correo_electronico AS email
+    fc.correo_electronico AS email
 FROM
-    flores_comahue f
+    flores_comahue fc
 LEFT JOIN
     localidad2 l
 ON
-    f.codigo_postal = l.codigo_postal::text
+    fc.codigo_postal = l.codigo_postal::text
 WHERE
-    universidad = UPPER('Univ. Nacional Del Comahue')
+    fc.universidad = UPPER('Univ. Nacional Del Comahue')
 AND
-    TO_DATE(f.fecha_de_inscripcion,'YYYY-MM-DD')
+    TO_DATE(fc.fecha_de_inscripcion,'YYYY-MM-DD')
         BETWEEN
             TO_DATE('01/09/2020','DD/MM/YYYY')
         AND
@@ -57,26 +57,26 @@ AND
 
 -- Universidad Del Salvador
 SELECT
-    f.universidad AS university,
-    f.carrera AS career,
-    f.fecha_de_inscripcion AS inscription_date,
-    split_part(f.nombre, '_', 1) AS first_name,
-    split_part(f.nombre, '_', 2) AS last_name,
-    f.sexo AS gender,
-    date_part('year',age(TO_DATE(f.fecha_nacimiento,'DD-Mon-YY'))) AS age,
+    svm.universidad AS university,
+    svm.carrera AS career,
+    svm.fecha_de_inscripcion AS inscription_date,
+    SPLIT_PART(svm.nombre, '_', 1) AS first_name,
+    SPLIT_PART(svm.nombre, '_', 2) AS last_name,
+    svm.sexo AS gender,
+    DATE_PART('year', AGE(TO_DATE(svm.fecha_nacimiento,'DD-Mon-YY'))) AS age,
     l.codigo_postal AS postal_code,
     l.localidad AS location,
-    f.email
+    svm.email
 FROM
-    salvador_villa_maria f
+    salvador_villa_maria svm
 LEFT JOIN
     localidad2 l
 ON
-    f.localidad = REPLACE(l.localidad, ' ', '_')
+    svm.localidad = REPLACE(l.localidad, ' ', '_')
 WHERE
-    universidad = REPLACE(UPPER('Universidad Del Salvador'), ' ', '_')
+    svm.universidad = REPLACE(UPPER('Universidad Del Salvador'), ' ', '_')
 AND
-    TO_DATE(f.fecha_de_inscripcion,'DD-Mon-YY')
+    TO_DATE(svm.fecha_de_inscripcion,'DD-Mon-YY')
         BETWEEN
             TO_DATE('01/09/2020','DD/MM/YYYY')
         AND
