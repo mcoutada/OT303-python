aaa11d84ec5e2c7b956fe0005c93d1c9732da9c8
nicomoya321 <niconob07@hotmail.com>
2022-09-16 10:05:25 -0300

    first commit

---
+++ OT303-19.sql
@@ -0,0 +1,58 @@
+
+
+-- jujuy_utn--
+	SELECT
+    f.university AS university,
+    f.career AS career,
+    f.inscription_date AS inscription_date,
+    split_part(f.nombre, ' ', 1) AS first_name,
+    split_part(f.nombre, ' ', 2) AS last_name,
+    f.sexo AS gender,
+    date_part('year',age(TO_DATE(f.birth_date,'DD-MM-YYYY'))) AS age,
+    l.codigo_postal AS postal_code,
+    f.location AS location,
+    f.email AS email,
+	 DATE_PART(
+        'year',
+        AGE(
+            CASE
+                WHEN
+                    TO_DATE(f.birth_date,'DD-MM-YY') > NOW()
+                THEN
+                    TO_DATE(f.birth_date,'DD-MM-YY') - interval '100 year'
+                ELSE
+                    TO_DATE(f.birth_date,'DD-MM-YY')
+            END  )) AS age
+FROM jujuy_utn f
+INNER JOIN localidad2 l
+ON l.codigo_postal= l.codigo_postal::text
+WHERE university = 'universidad_tecnologica_nacional'
+AND TO_DATE(f.inscription_date,'DD-Mon-YY')
+BETWEEN TO_DATE('19/01/2020','DD/MM/YYYY') AND TO_DATE('01/02/2021','DD/MM/YYYY');
+
+--universidad de tres de febrero--
+
+SELECT f.universidad AS university, f.careers AS career,f.fecha_de_inscripcion AS inscription_date,
+    split_part(f.names, '_', 1) AS first_name,
+    split_part(f.names, '_', 2) AS last_name,
+    f.sexo AS gender,
+    l.codigo_postal AS postal_code,
+    l.localidad AS location, f.correos_electronicos as correos_electronicos,
+	 DATE_PART(
+        'year',
+        AGE(
+            CASE
+                WHEN
+                    TO_DATE(f.birth_dates,'DD-MM-YY') > NOW()
+                THEN 
+                    TO_DATE(f.birth_dates,'DD-MM-YY') - interval '100 year'
+                ELSE 
+                    TO_DATE(f.birth_dates,'DD-MM-YY')
+            END  )) AS age 
+FROM palermo_tres_de_febrero f
+INNER JOIN localidad2 l
+ON CAST(f.codigo_postal AS INT) = l.codigo_postal
+WHERE universidad = 'universidad_nacional_de_tres_de_febrero'
+AND TO_DATE(f.fecha_de_inscripcion,'YYYY-MM-DD')
+        BETWEEN TO_DATE('01/09/2020','DD/MM/YY')
+        AND TO_DATE('01/02/2021','DD/MM/YY');
