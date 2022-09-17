e98beb337096b31f3abe0e3468a93b3d41315a6f
nicolasmoya <niconob07@hotmail.com>
2022-09-16 23:28:50 -0300

    first commit

--- OT303-19.sql
+++ OT303-19.sql
@@ -14,28 +14,31 @@
     split_part(f.nombre, ' ', 1) AS first_name,
     split_part(f.nombre, ' ', 2) AS last_name,
     f.sexo AS gender,
-    date_part('year',age(TO_DATE(f.birth_date,'DD-MM-YYYY'))) AS age,
+    date_part('year',age(TO_DATE(f.birth_date,'YYYY-MM-DD'))) AS age,
     l.codigo_postal AS postal_code,
     f.location AS location,
-    f.email AS email,
-	--se agrego un case debido a que muchas fechas estaban en texto y no en entero--
-	 DATE_PART(
-        'year',
-        AGE(
-            CASE
-                WHEN
-                    TO_DATE(f.birth_date,'DD-MM-YY') > NOW()
-                THEN
-                    TO_DATE(f.birth_date,'DD-MM-YY') - interval '100 year'
-                ELSE
-                    TO_DATE(f.birth_date,'DD-MM-YY')
-            END  )) AS age
+    f.email AS email	
 FROM jujuy_utn f
-INNER JOIN localidad2 l
-ON l.codigo_postal= l.codigo_postal::text
-WHERE university = 'universidad_tecnologica_nacional'
-AND TO_DATE(f.inscription_date,'DD-Mon-YY')
+LEFT JOIN localidad2 l
+ON upper(l.localidad)= l.localidad
+WHERE university LIKE 'universidad_tecnologica_nacional'
+AND TO_DATE(f.inscription_date,'YYYY-MM-DD')
 BETWEEN TO_DATE('19/01/2020','DD/MM/YYYY') AND TO_DATE('01/02/2021','DD/MM/YYYY');
+
+
+-- funcion donde se limita la edad/rango edad--
+CREATE OR REPLACE FUNCTION calculate_age(p.birth_dates DATE) RETURNS integer
+	DECLARE 
+		born_year integer := date_part('year', (p.birth_dates);
+		current_year integer := date_part('year', CURRENT_DATE);
+	BEGIN
+		IF born_year > current_year THEN
+			RETURN current_year - (born_year - 100);
+		ELSE
+			RETURN ROUND((CURRENT_DATE - (p.birth_dates)/365.25);
+		END IF;
+	END
+
 
 --universidad de tres de febrero--
 
@@ -44,22 +47,12 @@ SELECT f.universidad AS university, f.careers AS career,f.fecha_de_inscripcion A
     split_part(f.names, '_', 2) AS last_name,
     f.sexo AS gender,
     l.codigo_postal AS postal_code,
-    l.localidad AS location, f.correos_electronicos as correos_electronicos,
-	 DATE_PART(
-        'year',
-        AGE(
-            CASE
-                WHEN
-                    TO_DATE(f.birth_dates,'DD-MM-YY') > NOW()
-                THEN 
-                    TO_DATE(f.birth_dates,'DD-MM-YY') - interval '100 year'
-                ELSE 
-                    TO_DATE(f.birth_dates,'DD-MM-YY')
-            END  )) AS age 
+    l.localidad AS location, f.correos_electronicos as correos_electronicos
+	
 FROM palermo_tres_de_febrero f
-INNER JOIN localidad2 l
-ON CAST(f.codigo_postal AS INT) = l.codigo_postal
-WHERE universidad = 'universidad_nacional_de_tres_de_febrero'
-AND TO_DATE(f.fecha_de_inscripcion,'YYYY-MM-DD')
+LEFT JOIN localidad2 l
+ON f.codigo_postal::INT = l.codigo_postal
+WHERE universidad LIKE 'universidad_nacional_de_tres_de_febrero'
+AND TO_DATE(f.fecha_de_inscripcion,'DD-Mon-YY')
         BETWEEN TO_DATE('01/09/2020','DD/MM/YY')
         AND TO_DATE('01/02/2021','DD/MM/YY');
