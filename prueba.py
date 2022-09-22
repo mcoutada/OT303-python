3991688c2fe8f194e995fe49a46dd57705b3d0c6
MartinCifre <tinchoc8@gmail.com>
2022-09-22 17:14:28 -0300

    eliminacion prueba logs

--- prueba.py
+++
@@ -1,8 +0,0 @@
-import logging
-from datetime import datetime
-from logger import set_logger
-
-log_name= "prueba" + datetime.today().strftime('%Y-%m-%d')
-
-logger=set_logger(name_logger=log_name)
-logger.info("funciona?")
