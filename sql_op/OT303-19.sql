875078f4cc783ee9bb8ac4c309f2454688c9f77f
nicolasmoya <niconob07@hotmail.com>
2022-09-22 22:37:29 -0300

    sql extractor

--- sql_op/OT303-19.sql
+++ sql_op/OT303-19.sql
@@ -7,25 +7,29 @@
 --Universidad Nacional De Tres De Febrero
 
 -- jujuy_utn--
-	SELECT
-    f.university AS university,
-    f.career AS career,
-    f.inscription_date AS inscription_date,
-    split_part(f.nombre, ' ', 1) AS first_name,
-    split_part(f.nombre, ' ', 2) AS last_name,
-    f.sexo AS gender,
-    date_part('year',age(TO_DATE(f.birth_date,'YYYY-MM-DD'))) AS age,
-    l.codigo_postal AS postal_code,
-    f.location AS location,
-    f.email AS email	
-FROM jujuy_utn f
-LEFT JOIN localidad2 l
-ON upper(l.localidad)= l.localidad
-WHERE university LIKE 'universidad_tecnologica_nacional'
-AND TO_DATE(f.inscription_date,'YYYY-MM-DD')
-BETWEEN TO_DATE('19/01/2020','DD/MM/YYYY') AND TO_DATE('01/02/2021','DD/MM/YYYY');
-
-
+SELECT 
+	ju.university, 
+	ju.career, 
+	ju.inscription_date,
+	split_part(ju.nombre,' ',1) AS first_name,
+	split_part(ju.nombre ,' ',2) AS last_name,
+	ju.sexo as gender,
+	date_part('year',age(TO_DATE(ju.birth_date,'YYYY-MM-DD'))) AS age,
+	l.codigo_postal as postal_code,
+	 ju.location as location,
+	email  
+FROM jujuy_utn ju 
+left join localidad2 l 
+on 
+upper(ju.location)  = l.localidad
+where university  like 'universidad nacional de jujuy'
+and 
+ 	TO_DATE(inscription_date,'YYYY-MM-DD')
+        BETWEEN
+            TO_DATE('01/09/2020','DD/MM/YYYY')
+        AND
+            TO_DATE('01/02/2021','DD/MM/YYYY')
+;
 -- funcion donde se limita la edad/rango edad--
 CREATE OR REPLACE FUNCTION calculate_age(p.birth_dates DATE) RETURNS integer
 	DECLARE 
@@ -54,5 +58,5 @@ LEFT JOIN localidad2 l
 ON f.codigo_postal::INT = l.codigo_postal
 WHERE universidad LIKE 'universidad_nacional_de_tres_de_febrero'
 AND TO_DATE(f.fecha_de_inscripcion,'DD-Mon-YY')
-        BETWEEN TO_DATE('01/09/2020','DD/MM/YY')
-        AND TO_DATE('01/02/2021','DD/MM/YY');
+        BETWEEN TO_DATE('01/09/2020','DD/MM/YYYY')
+        AND TO_DATE('01/02/2021','DD/MM/YYYY');
