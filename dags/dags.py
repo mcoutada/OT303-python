0333586a05a32825de5d0dc23729c51cf2dc8d00
nicolasmoya <niconob07@hotmail.com>
2022-09-22 23:05:09 -0300

    dags

--- dags/dags.py
+++ dags/dags.py
@@ -4,21 +4,56 @@ from datetime import timedelta, datetime
 
 print(datetime.today)
 
-with DAG(
-        "DAG_universidades_C",
-        description="DAG_universidades_C",
-        schedule_interval=timedelta(hours=1),
-        start_date=datetime.today()
+with DAG    (
+            "DAG_universidades_A",
+            description = "DAG_universidades_A",
+            schedule_interval = timedelta(hours=1),
+            start_date = datetime.today()
+                ) as dag:
 
-        ) as dag:
-    u_jujuy_extract = DummyOperator(task_id="u_jujuy_extract")
-    u_jujuy_transform = DummyOperator(task_id="u_jujuy_transform")
-    u_jujuy_load = DummyOperator(task_id="u_jujuy_load")
+             run_this_first = DummyOperator (
+            task_id = 'branching',
+             dag = dag,
+             )
 
-    [u_jujuy_extract >> u_jujuy_transform >> u_jujuy_load]
+     options = ['DAG_universidades_A'] ,
 
-    u_palermo_extract = DummyOperator(task_id="u_palermo_extract")
-    u_palermo_transform = DummyOperator(task_id="u_palermo_transform")
-    u_palermo_load = DummyOperator(task_id="u_palermo_load")
+        branching = BranchPythonOperator(
+            task_id = 'branching',
+            python_callable = lambda: random.choice(DAG_universidades_A),
+            dag = dag,
+            )
 
-    [u_palermo_extract >> u_palermo_transform >> u_palermo_load]
+            run_this_first >> branching
+
+                            join = DummyOperator(
+                task_id = 'enable_system',
+                trigger_rule = 'one_success',
+                dag = dag,
+            )
+
+                     for option in options:
+                 t = DummyOperator(
+                task_id = option,
+                dag = dag,
+             )
+
+                dummy_follow = DummyOperator (
+                task_id = 'follow_' + option,
+                dag = dag ,
+            )
+
+
+branching >> t >> dummy_follow >> join
+
+u_tecnologica_nacional_extract = DummyOperator(task_id="u_tecnologica_nacional_extract") ,
+u_tecnologica_nacional_transform = DummyOperator(task_id="u_tecnologica_nacional_transform"),
+u_tecnologica_nacional_load = DummyOperator(task_id="u_tecnologica_nacional_load"),
+
+[u_tecnologica_nacional_extract >> u_tecnologica_nacional_transform >> u_tecnologica_nacional_load]
+
+u_tres_de_febrero_extract = DummyOperator(task_id=" u_tres_de_febrero_extract")
+u_tres_de_febrero_transform = DummyOperator(task_id=" u_tres_de_febrero_transform")
+u_tres_de_febrero_load = DummyOperator(task_id=" u_tres_de_febrero_load")
+
+[u_tres_de_febrero_extract >>  u_tres_de_febrero_transform >> u_tres_de_febrero_load]
