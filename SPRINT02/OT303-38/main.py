899dd1633ee52484854eb5f91833c17c0eb756d0
Mariano Ismael Coutada <mcoutada@gmail.com>
2022-09-23 13:16:42 -0300

    add issue description

--- SPRINT02/OT303-38/main.py
+++ SPRINT02/OT303-38/main.py
@@ -1,3 +1,24 @@
+'''
+https://alkemy-labs.atlassian.net/browse/OT303-38
+
+COMO: Analista de datos
+QUIERO: Configurar los log
+PARA: Mostrarlos en consola
+
+Criterios de aceptación: 
+
+Configurar logs para Univ. Nacional Del Comahue
+
+Configurar logs para Universidad del Salvador
+
+Utilizar la librería de Loggin de python: https://docs.python.org/3/howto/logging.html
+
+Realizar un log al empezar cada DAG con el nombre del logger
+
+Formato del log: %Y-%m-%d - nombre_logger - mensaje
+Aclaración:
+Deben dejar la configuración lista para que se pueda incluir dentro de las funciones futuras. No es necesario empezar a escribir logs.
+'''
 import include.logger as logger
 import sys
 
