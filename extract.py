dafceb35005ac56c03ceb9bf5c74801687ddd419
MartinCifre <tinchoc8@gmail.com>
2022-09-25 13:24:01 -0300

    funcion extract

--- extract.py
+++ extract.py
@@ -1,9 +1,16 @@
+from datetime import datetime, timedelta
+import os
 import pandas as pd
 from sqlalchemy import create_engine
 from logger import set_logger
 from DB_connection import get_engine
 
-engine = get_engine() 
-query = open("SQL/u_de_jujuy.sql", 'r').read()
-df = pd.read_sql(query, engine)
-df.to_csv("u_de_jujuy.csv", index=False)
+
+def extract():
+        engine = get_engine() 
+        query = open("SQL/u_de_jujuy.sql", 'r').read()
+        df = pd.read_sql(query, engine)
+        df.to_csv("files/u_de_jujuy.csv", index=False)
+
+extract()    
+logger.info("Extraccion finalizada")
