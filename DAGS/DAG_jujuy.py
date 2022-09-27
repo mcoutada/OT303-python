b0ccc6a5132ced9f0c759aaa2af36b70b5242e09
MartinCifre <tinchoc8@gmail.com>
2022-09-27 16:35:18 -0300

    DAGs

--- DAGS/DAG_jujuy.py
+++ DAGS/DAG_jujuy.py
@@ -9,36 +9,40 @@ from logger import set_logger
 from DB_connection import get_engine
 
 # Logs configuration for the dag
-log_name= LOG_NAME + datetime.today().strftime('%Y-%m-%d')
+log_name= "LOG_NAME" + datetime.today().strftime('%Y-%m-%d')
 logger=set_logger(name_logger=log_name)
 logger.info("DAG started")
 
-universidad = "u_de_palermo"
+universidad = "u_de_jujuy"
 
 def extract_data():
+    """Python operator to extract data
+    """
     extract()
 
 def transform():
+     """Python operator to transform data
+    """
     normalization()
 
-with DAG(
-    "DAG_palermo",
-    description="DAG_palermo",
-    schedule_interval=timedelta(hours=1), 
-    start_date=datetime.today() 
-    
-) as dag:
 
-    u_palermo_extract = PythonOperator(task_id="u_palermo_extract",  # Id for the task
+with DAG(
+    "DAG_jujuy",
+    description="DAG_jujuy",
+    schedule_interval=None, 
+    start_date=datetime.today()     
+
+) as dag:
+    u_jujuy_extract = PythonOperator(task_id="u_jujuy_extract",  # Id for the task
                         python_callable=extract_data,  # Execution task (extract function)
                         provide_context=True  # For share data)
     )                    
 
-    u_palermo_transform = PythonOperator(task_id="u_palermo_transform",
+    u_jujuy_transform = PythonOperator(task_id="u_jujuy_transform",
                           python_callable=transform,  # Execution task (transform function)
                           provide_context=True  # For share data
                           )
 
-    u_palermo_load = DummyOperator(task_id="u_palermo_load")
-
-    u_palermo_extract >> u_palermo_transform >> u_palermo_load
+    u_jujuy_load = DummyOperator(task_id="u_jujuy_load")
+       
+    u_jujuy_extract >> u_jujuy_transform >> u_jujuy_load
