7fc73102d241a9c9cad309da9e94e2a044ca4694
avalos-p <p2727andres@gmail.com>
2022-09-30 17:56:32 -0300

    logger V2

--- dags/functions/logger_universidades.py
+++ dags/functions/logger_universidades.py
@@ -1,8 +1,20 @@
 import logging
 def logger_universidades():
-    logging.basicConfig(
-    filename="info.log",
-    format='%(asctime)s - nombre_logger - %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d')
-    return print("Logger activo ...")
+    "Same logger for both Dags"
+    logger = logging.getLogger("logs_univ")
+
+    S_handler = logging.StreamHandler()
+    logger.addHandler(S_handler)
+
+
+    # Adding format to logger
+    # Setting lvl to info
+    format = logging.Formatter(
+        fmt='%(asctime)s - nombre_logger - %(message)s',
+        datefmt ='%Y-%m-%d')
+    S_handler.setFormatter(format)        
+    logger.setLevel(logging.INFO)
+
+    return logger
+    
+
