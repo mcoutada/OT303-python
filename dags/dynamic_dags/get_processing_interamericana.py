4c23d274d1a69d858d70fe9b0b53cfd007b1f40a
avalos-p <p2727andres@gmail.com>
2022-10-05 19:30:40 -0300

    dynamic_dags added

---
+++ dags/dynamic_dags/get_processing_interamericana.py
@@ -0,0 +1,25 @@
+from sched import scheduler
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime
+
+with DAG(dag_id="get_processing_interamericana",
+    start_date=datetime(2022, 10, 6),
+    schedule_interval='@hourly',
+    catchup=False) as dag:
+
+    @task
+    def extract_univ(symbol):
+        return symbol
+        
+
+    @task
+    def proces_univ(symbol):
+        return symbol
+
+    @task
+    def upload_univ(symbol):
+        return symbol
+        
+    upload_univ(proces_univ(extract_univ()))
+        
